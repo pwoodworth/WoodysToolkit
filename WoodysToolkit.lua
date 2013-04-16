a160b2bcc5adbe5e59f84f7f01ffa7c7170b1319
Patrick Woodworth <patrick@woodworth.org>
2013-04-16 16:26:12 -0700

    Using data local var

--- WoodysToolkit.lua
+++ WoodysToolkit.lua
@@ -13,17 +13,26 @@ BINDING_NAME_WoodysToolkit_mode_enable  = "Enable MouseLook Lock"
 BINDING_NAME_WoodysToolkit_mode_disable  = "Disable MouseLook Lock"
 BINDING_NAME_WoodysToolkit_momentary    = "Disable Lock While Pressed"
 
-WoodysToolkit = setmetatable({}, {
+WoodysToolkit = {}
+WoodysToolkit.data = setmetatable({}, {
     __index = function(table, key)
+        if type(WoodysToolkit_acctData) ~= "table" then
+            WoodysToolkit_acctData = {}
+        end
         return WoodysToolkit_acctData[key]
     end,
     __newindex = function(table, key, value)
+        if type(WoodysToolkit_acctData) ~= "table" then
+            WoodysToolkit_acctData = {}
+        end
         WoodysToolkit_acctData[key] = value
     end
 })
 
+local data = WoodysToolkit.data
+
 local function WTK_debug(...)
-    if not DEFAULT_CHAT_FRAME or not WoodysToolkit.debug then return end
+    if not DEFAULT_CHAT_FRAME or not data.debug then return end
     local msg = ''
     for k,v in ipairs(arg) do
         msg = msg .. tostring(v) .. ' : '
@@ -43,7 +52,7 @@ end
 
 local function WoodysToolkit_GetButton1Action()
     local b1name = "WoodysToolkit_mode_disable"
-    if WoodysToolkit.btn1backsup then
+    if data.btn1backsup then
         b1name = "MOVEBACKWARD"
     end
     return b1name
@@ -56,7 +65,7 @@ local function WoodysToolkit_InitBindings()
 end
 
 local function WoodysToolkit_ApplyMode()
-    local shouldBeLooking = WoodysToolkit.lockEnabled and not WoodysToolkit.lockSuppressed
+    local shouldBeLooking = data.lockEnabled and not data.lockSuppressed
     if IsMouselooking() then
       if not shouldBeLooking then
         MouselookStop()
@@ -70,25 +79,27 @@ end
 
 function WoodysToolkit_Reset()
     WoodysToolkit_acctData = {}
+    WoodysToolkit_InitBindings()
+    WoodysToolkit_ApplyMode()
 end
 
 function WoodysToolkit_Enable()
-    WoodysToolkit.lockEnabled = true
+    data.lockEnabled = true
     WoodysToolkit_ApplyMode()
 end
 
 function WoodysToolkit_Disable()
-    WoodysToolkit.lockEnabled = false
+    data.lockEnabled = false
     WoodysToolkit_ApplyMode()
 end
 
 function WoodysToolkit_Toggle()
-    WoodysToolkit.lockEnabled = not WoodysToolkit.lockEnabled
+    data.lockEnabled = not data.lockEnabled
     WoodysToolkit_ApplyMode()
 end
 
 function WoodysToolkit_Momentary(keystate)
-    WoodysToolkit.lockSuppressed = (keystate == "down")
+    data.lockSuppressed = (keystate == "down")
     WoodysToolkit_ApplyMode()
 end
 
@@ -100,13 +111,13 @@ function WoodysToolkit_SlashCommand(msg, editbox)
         WoodysToolkit_Reset()
     elseif command == "button1" then
         if rest == "backup" then
-            WoodysToolkit.btn1backsup = true
+            data.btn1backsup = true
             WoodysToolkit_InitBindings()
         elseif rest == "cancel" then
-            WoodysToolkit.btn1backsup = false
+            data.btn1backsup = false
             WoodysToolkit_InitBindings()
         else
-            local curval = WoodysToolkit.btn1backsup and 'backup' or 'cancel'
+            local curval = data.btn1backsup and 'backup' or 'cancel'
             print("button1 : ", curval)
         end
     elseif command == "remove" and rest ~= "" then
@@ -120,8 +131,8 @@ function WoodysToolkit_SlashCommand(msg, editbox)
 end
 
 function WoodysToolkit_OnLoad(self,...)
+    WoodysToolkitFrame:RegisterEvent("ADDON_LOADED")
     WoodysToolkitFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-    WoodysToolkit_InitBindings()
     SLASH_WoodysToolkit1, SLASH_WoodysToolkit2 = '/woodystoolkit', "/wtk";
     SlashCmdList["WoodysToolkit"] = WoodysToolkit_SlashCommand; -- Also a valid assignment strategy
 end;
@@ -129,7 +140,6 @@ end;
 function WoodysToolkit_OnEvent(self,event,...)
     --Print("on event:" ..event)
     if event == "PLAYER_ENTERING_WORLD" then
-        --Print("PLAYER_ENTERING_WORLD event")
         WoodysToolkit_InitBindings()
         WoodysToolkit_ApplyMode()
     end
