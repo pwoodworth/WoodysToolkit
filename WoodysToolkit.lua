da30c93bbb8eb9c894e4b02c210f670a990d1e7d
Patrick Woodworth <patrick@woodworth.org>
2013-04-26 00:28:49 -0700

    Tweaks

--- WoodysToolkit.lua
+++ WoodysToolkit.lua
@@ -17,6 +17,9 @@ WoodysToolkit_acctData = {}
 
 -- WoodysToolkit = {}
 
+local WTK_DEBUG = false
+
+local statedata = {}
 
 local function WoodysToolkit_InitData()
     if type(WoodysToolkit_acctData) ~= "table" then
@@ -29,10 +32,16 @@ end
 
 local data = setmetatable({}, {
     __index = function(table, key)
+        if key == "debug" then
+            return WTK_DEBUG
+        end
         WoodysToolkit_InitData()
         return WoodysToolkit_acctData[key]
     end,
     __newindex = function(table, key, value)
+        if key == "debug" then
+            return
+        end
         WoodysToolkit_InitData()
         WoodysToolkit_acctData[key] = value
     end
@@ -56,6 +65,7 @@ WoodysToolkit = setmetatable({}, {
 WoodysToolkit.OVERRIDE_DEFAULTS = {
     BUTTON1 = "WoodysToolkit_mode_disable",
     BUTTON2 = "MOVEFORWARD",
+    BUTTON3 = "TOGGLEAUTORUN",
     LEFT = "STRAFELEFT",
     RIGHT = "STRAFERIGHT",
 }
@@ -71,6 +81,11 @@ end
 
 local function Print(text)
     if not DEFAULT_CHAT_FRAME then return end
+    DEFAULT_CHAT_FRAME:AddMessage(text)
+end
+
+local function dprint(text)
+    if not DEFAULT_CHAT_FRAME or not data.debug then return end
     DEFAULT_CHAT_FRAME:AddMessage(text)
 end
 
@@ -91,14 +106,10 @@ end
 
 local function WoodysToolkit_ApplyMode()
     local shouldBeLooking = data.lockEnabled and not data.lockSuppressed
-    if IsMouselooking() then
-      if not shouldBeLooking then
-        MouselookStop()
-      end
-    else
-      if shouldBeLooking then
+    if shouldBeLooking then
         MouselookStart()
-      end
+    else
+        MouselookStop()
     end
 end
 
@@ -114,13 +125,17 @@ function WoodysToolkit_Enable()
 end
 
 function WoodysToolkit_Disable()
+    if statedata.moving then return end
     data.lockEnabled = false
     WoodysToolkit_ApplyMode()
 end
 
 function WoodysToolkit_Toggle()
-    data.lockEnabled = not data.lockEnabled
-    WoodysToolkit_ApplyMode()
+    if data.lockEnabled then
+        WoodysToolkit_Disable()
+    else
+        WoodysToolkit_Enable()
+    end
 end
 
 function WoodysToolkit_Momentary(keystate)
@@ -169,6 +184,70 @@ function WoodysToolkit_OnEvent(self,event,...)
         WoodysToolkit_ApplyMode()
     end
 end
+
+function WoodysToolkit_MoveAndSteerStart()
+    statedata.steering = true
+    dprint('statedata.steering: ' .. status(statedata.steering))
+end
+
+function WoodysToolkit_MoveAndSteerStop()
+    statedata.steering = false
+    WoodysToolkit_Disable()
+    dprint('statedata.steering: ' .. status(statedata.steering))
+end
+
+function WoodysToolkit_TurnOrActionStart()
+    statedata.turning = true
+    dprint('statedata.turning: ' .. status(statedata.turning))
+end
+
+function WoodysToolkit_TurnOrActionStop()
+    statedata.turning = false
+    dprint('statedata.turning: ' .. status(statedata.turning))
+end
+
+function WoodysToolkit_CameraOrSelectOrMoveStart()
+    statedata.camera = true
+    dprint('statedata.camera: ' .. status(statedata.camera))
+end
+
+function WoodysToolkit_CameraOrSelectOrMoveStop()
+    statedata.camera = false
+    dprint('statedata.camera: ' .. status(statedata.camera))
+end
+
+function WoodysToolkit_MoveForwardStart()
+    statedata.moving = true
+    dprint('statedata.moving1: ' .. status(statedata.moving))
+end
+
+function WoodysToolkit_MoveForwardStop()
+    statedata.moving = false
+    dprint('statedata.moving1: ' .. status(statedata.moving))
+end
+
+function WoodysToolkit_MoveBackwardStart()
+    statedata.moving = true
+    dprint('statedata.moving2: ' .. status(statedata.moving))
+end
+
+function WoodysToolkit_MoveBackwardStop()
+    statedata.moving = false
+    dprint('statedata.moving2: ' .. status(statedata.moving))
+end
+
+--[[
+--]]
+hooksecurefunc("MoveAndSteerStart", WoodysToolkit_MoveAndSteerStart);
+hooksecurefunc("TurnOrActionStart", WoodysToolkit_TurnOrActionStart);
+hooksecurefunc("TurnOrActionStop", WoodysToolkit_TurnOrActionStop);
+hooksecurefunc("CameraOrSelectOrMoveStart", WoodysToolkit_CameraOrSelectOrMoveStart);
+hooksecurefunc("CameraOrSelectOrMoveStop", WoodysToolkit_CameraOrSelectOrMoveStop);
+hooksecurefunc("MoveAndSteerStop", WoodysToolkit_MoveAndSteerStop);
+hooksecurefunc("MoveForwardStart", WoodysToolkit_MoveForwardStart);
+hooksecurefunc("MoveForwardStop", WoodysToolkit_MoveForwardStop);
+hooksecurefunc("MoveBackwardStart", WoodysToolkit_MoveBackwardStart);
+hooksecurefunc("MoveBackwardStop", WoodysToolkit_MoveBackwardStop);
 
 function is_main(_arg, ...)
     local n_arg = _arg and #_arg or 0;
