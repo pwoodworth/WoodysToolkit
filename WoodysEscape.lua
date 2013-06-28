31ef3fd255fb3090026fe2d7fe763325a6abfade
Patrick Woodworth <patrick@woodworth.org>
2013-06-27 23:57:39 -0700

    Adding WoodysEscape functionality

---
+++ WoodysEscape.lua
@@ -0,0 +1,7 @@
+--[[------------------------------------------------------------
+    Woodys Escape v1.0.50200
+
+    By Somdubai
+--------------------------------------------------------------]]
+local b = CreateFrame("Button", "WoodysEscapeButton", UIParent, "SecureActionButtonTemplate")
+b:SetAttribute("type", "stop")
