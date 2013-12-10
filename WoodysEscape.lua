3908fb46bc9dae6394db2054b8768df57f69e827
Patrick Woodworth <patrick@woodworth.org>
2013-12-09 16:19:49 -0800

    WIP

--- WoodysEscape.lua
+++ WoodysEscape.lua
@@ -1,7 +1,4 @@
---[[------------------------------------------------------------
-    Woodys Escape v1.0.50200
-
-    By Somdubai
---------------------------------------------------------------]]
 local b = CreateFrame("Button", "WoodysEscapeButton", UIParent, "SecureActionButtonTemplate")
 b:SetAttribute("type", "stop")
+
+C_StorePublic.IsDisabledByParentalControls = function () return false end
