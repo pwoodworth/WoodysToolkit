31a6d1f5b52ab776a1be0089e2007dc307fbef04
Patrick Woodworth <patrick@woodworth.org>
2014-02-12 15:38:34 -0800

    Add notes

---
+++ foo.lua
@@ -0,0 +1,22 @@
+that = function(event, _, logevent, _, ...)
+  local reverse = false
+  local trigger = false
+  local now = GetTime()
+  local start, _, enable = GetInventoryItemCooldown("player", 10)
+  local castart, caduration, caenabled = GetSpellCooldown("Celestial Alignment")
+  local caleft = 0
+  if enable and start and castart and start == 0 and castart > 0 then
+    caleft = ((castart + caduration) - now)
+    if (caleft > 60) then
+      trigger = true
+    end
+  end
+  if trigger ~= reverse then
+    -- local prefix = trigger and "TRIGGER" or "UNTRIGGER"
+    -- print(prefix.." caleft: "..caleft)
+    return true
+  end
+  return false
+end
+
+-- Tempus Repit  -- Sinister Primal Diamond
