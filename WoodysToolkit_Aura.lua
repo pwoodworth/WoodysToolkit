7fa54f1fe0da952f90741a15662ff12063cb54f7
Patrick Woodworth <patrick@woodworth.org>
2014-03-06 01:00:02 -0800

    WIP

--- WoodysToolkit_Aura.lua
+++ WoodysToolkit_Aura.lua
@@ -11,14 +11,38 @@ setfenv(1, SUB)
 -- Auras
 --------------------------------------------------------------------------------
 
+local function isAvailableWithin(spellname, seconds)
+  local castart, caduration, caenabled = GetSpellCooldown(spellname)
+  if castart and castart > 0 then
+    local now = GetTime()
+    local caleft = ((castart + caduration) - now)
+    if (caleft > seconds) then
+      return false
+    end
+  end
+  return true
+end
+
+
 --[[
 COMBAT_LOG_EVENT_UNFILTERED, PLAYER_ENTERING_WORLD, ACTIONBAR_UPDATE_COOLDOWN, SPELL_UPDATE_COOLDOWN
 --]]
 
 _G.WoodysAuraTool = {
   synapse = {
-    trigger = function(reverse, event, _, logevent, _, ...)
---    local reverse = false
+    trigger = function(reverse, ...)
+      local trigger = false
+      local start, _, enable = GetInventoryItemCooldown("player", 10)
+      if enable and start and start == 0 then
+        trigger = not isAvailableWithin("Celestial Alignment", 60)
+      end
+      if trigger ~= reverse then
+        return true
+      end
+      return false
+    end,
+    trigger2 = function(reverse, event, _, logevent, _, ...)
+    --    local reverse = false
       local trigger = false
       local now = GetTime()
       local start, _, enable = GetInventoryItemCooldown("player", 10)
@@ -36,7 +60,7 @@ _G.WoodysAuraTool = {
         return true
       end
       return false
-    end
+    end,
   }
 }
 
